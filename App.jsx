d74325fa07af578263987396280c67e9d120f7a4
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-05-05 17:47:13 +0300

    Initialize App component with project data

--- App.jsx
+++ App.jsx
@@ -3,4 +3,11 @@ import ProjCard from "./Proj/ProjCard";
 import ProjList from "./Proj/ProjList";
 import Form from "./Proj/Form";
 
-function App() {}
+function App() {
+  const projects = [
+    { title: "Project 1", description: "Description of Project 1" },
+    { title: "Project 2", description: "Description of Project 2" },
+    { title: "Project 3", description: "Description of Project 3" },
+  ];
+}
+}
