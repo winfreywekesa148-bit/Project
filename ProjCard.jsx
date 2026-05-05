cb3b4a038a0a1794f91dedfb36acbd03e727e6fd
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-05-05 17:02:08 +0300

    Create ProjCard component for project display

---
+++ ProjCard.jsx
@@ -0,0 +1,11 @@
+function ProjCard ({ title, description }) {
+    return (
+        <div className="proj-card">
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <button>View Project</button>
+        </div>
+    );
+}
+
+export default ProjCard;
