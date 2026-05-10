4b85dfec06e96d7c13a08e28529eef43a72af599
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-05-10 14:14:17 +0300

    Refactor App component to include Submit and update projects
    Updated project list and added Submit component.

--- App/App.jsx
+++ App/App.jsx
@@ -1,22 +1,24 @@
 import Search from "./Proj/Search";
-import ProjCard from "./Proj/ProjCard";
+import Submit from "./Proj/Submit";
 import ProjList from "./Proj/ProjList";
-import Form from "./Proj/Form";
+import { useState } from "react";
 
 function App() {
   const projects = [
-    { title: "Project 1", description: "Description of Project 1" },
-    { title: "Project 2", description: "Description of Project 2" },
     { title: "Project 3", description: "Description of Project 3" },
+    { title: "Project 2", description: "Description of Project 2" },
+    { title: "Project 1", description: "Description of Project 1" },
   ];
 
+  const [projects, setProjects] = useState([]);
+  
   return (
-    <div className="App">
+    <div className="App"  style= {{backgroundImage: "url('https://www.pinterest.com/pin/1900024838134773/')"}}>
       <h1>My Projects</h1>
       <Search />
+      <Submit></Submit>
       <ProjList projects={projects} />
-      <Form />
-    </div>
+     </div>
   );
 
 }
