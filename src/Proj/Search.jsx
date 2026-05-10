76c7b1c72fd4dcb9b3973dbb7648a92f4881662b
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-05-10 14:19:04 +0300

    Enhance Search component with UI and functionality
    Added a search input and button with results display.

--- src/Proj/Search.jsx
+++ src/Proj/Search.jsx
@@ -12,9 +12,26 @@ function Search () {
 
   
     return (
-        <input type="text" placeholder="Search..." 
+        <div style={{ border: "1px solid #ccc", borderStyle: "dotted", padding: "16px", margin: "16px"}}>
+        <input id="search" type="text" placeholder="Search..." 
         onChange={(e) => console.log(e.target.value)}
         />
+
+        <button onClick={handleSearch}>Search</button> 
+
+        <br /> 
+
+        {searchResults.length > 0 && (
+            <div styles={{ fontSize:"larger", color:"white"}}>
+                <h2>Search Results:</h2>
+                <ul>
+                    {searchResults.map((result, index) => (
+                        <li key={index}>{result}</li>
+                    ))}
+                </ul>
+            </div>
+        )}
+        </div>
     );
 }
 
